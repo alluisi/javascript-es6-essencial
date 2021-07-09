337fec916a5832420e92e3ef1ba35df5f987ca93
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-07-09 14:18:10 -0300

    Currying

---
+++ 1-conceitos/5-currying/1-currying.js
@@ -0,0 +1,8 @@
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(2, 2));
+console.log(soma(2, 3));
+console.log(soma(2, 4));
+console.log(soma(2, 5));
---
+++ 1-conceitos/5-currying/2-currying.js
@@ -0,0 +1,12 @@
+function soma(a) {
+    return function (b) {
+        return a + b;
+    }
+}
+
+const soma2 = soma(2);
+
+console.log(soma2(2));
+console.log(soma2(3));
+console.log(soma2(4));
+console.log(soma2(5));
