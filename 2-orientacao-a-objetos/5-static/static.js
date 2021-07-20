cc8bb2c29970655821a5f356786cadfdd7d5dfbf
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-07-20 14:16:38 -0300

    Static

---
+++ 2-orientacao-a-objetos/5-static/static.js
@@ -0,0 +1,19 @@
+/*
+function Person() { }
+
+Person.walk = function () {
+    console.log('walking...');
+}
+
+console.log(Person.walk());
+// "walking..."
+*/
+
+class Person {
+    static walk() {
+        console.log('walking...');
+    }
+}
+
+console.log(Person.walk());
+// "walking..."
