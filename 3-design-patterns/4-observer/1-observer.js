13e968df1a02d56cdf634836745060ababa8f592
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-08-13 16:01:09 -0300

    Observer

---
+++ 3-design-patterns/4-observer/1-observer.js
@@ -0,0 +1,19 @@
+// EXEMPLO:
+
+class Observable {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(f) {
+        this.observers.push(f);
+    }
+
+    unsubscribe(f) {
+        this.observers = this.observers.filter(subscriber => subscriber !== f);
+    }
+
+    notify(data) {
+        this.observers.forEach(observer => observer(data));
+    }
+}
---
+++ 3-design-patterns/4-observer/2-observer.js
@@ -0,0 +1,45 @@
+class Observable {
+    constructor() {
+        this.observables = [];
+    }
+
+    subscribe(fn) {
+        this.observables.push(fn);
+    }
+
+    notify(data) {
+        this.observables.forEach(fn => fn(data));
+    }
+
+    unsubscribe(fn) {
+        this.observables = this.observables.filter(obs => obs !== fn);
+    }
+}
+
+const o = new Observable();
+
+const logData1 = data => console.log(`Subscribe 1: ${data}`);
+const logData2 = data => console.log(`Subscribe 2: ${data}`);
+const logData3 = data => console.log(`Subscribe 3: ${data}`);
+
+o.subscribe(logData1);
+o.subscribe(logData2);
+o.subscribe(logData3);
+
+o.notify('notified 1');
+/*
+Subscribe 1: notified 1
+Subscribe 2: notified 1
+Subscribe 3: notified 1
+*/
+
+o.unsubscribe(logData2);
+
+o.notify('notified 2');
+/*
+Subscribe 1: notified 1
+Subscribe 2: notified 1
+Subscribe 3: notified 1
+Subscribe 1: notified 2
+Subscribe 3: notified 2
+*/
