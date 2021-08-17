d328e92e96af1d000fc97ed8eac31144337a1f68
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-08-17 14:17:41 -0300

    Transformar

---
+++ 4-manipulacao-e-iteracao-de-arrays/6-tranformar/1-join.js
@@ -0,0 +1,4 @@
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.join('-'));
+// 1-2-3-4-5
---
+++ 4-manipulacao-e-iteracao-de-arrays/6-tranformar/2-reduce.js
@@ -0,0 +1,4 @@
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.reduce((total, value) => total += value, 0));
+// 15
